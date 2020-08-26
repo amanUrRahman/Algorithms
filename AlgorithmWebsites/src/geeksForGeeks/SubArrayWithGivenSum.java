7ba3263bfcdb9cee93f5981494485a6497f56a9b
AMARAHMA <AMARAHMA@AMARAHMA-IN.oradev.oraclecorp.com>
2020-08-26 21:33:14 +0530

    gfg-program to find subarray with specified sum

---
+++ AlgorithmWebsites/src/geeksForGeeks/SubArrayWithGivenSum.java
@@ -0,0 +1,35 @@
+package geeksForGeeks;
+
+public class SubArrayWithGivenSum {
+
+	private static void subArray(int[] array, int sum) {
+		int i, j;
+		for (i = 0, j = 0; i < array.length && j < array.length;) {
+			if (sum(array, i, j) < sum) {
+				j++;
+				continue;
+			} else if (sum(array, i, j) > sum) {
+				i++;
+				continue;
+			} else if (sum(array, i, j) == sum) {
+				System.out.println("The subarray is between " + (i + 1) + " and " + (j + 1));
+				return;
+			}
+		}
+		System.out.println("The sum is not present in the array");
+	}
+
+	private static int sum(int[] subArray, int i, int j) {
+		int sum = 0;
+		for (; i <= j; i++) {
+			sum = sum + subArray[i];
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		subArray(new int[] { 2, 3, 6, 9, 5, 3 }, 13);
+
+	}
+
+}
